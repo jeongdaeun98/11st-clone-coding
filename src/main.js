6bd7e4c5a255eaada5b063314070004dd1466fcd
jeongdaeun98 <uneap@sk.com>
2021-01-27 14:22:42 +0900

    feat: add lazyLoad for IntersectionObserver

--- src/main.js
+++ src/main.js
@@ -1,12 +1,15 @@
 import 'regenerator-runtime'
+import 'intersection-observer'
 import Vue from 'vue'
 import App from './App'
 import store from './store'
 import searchData from './plugins/searchData'
 import fetchData from './plugins/fetchData'
+import lazyLoad from "./plugins/lazyLoad";
 
 Vue.use(searchData)
 Vue.use(fetchData)
+Vue.use(lazyLoad)
 
 new Vue({
   el: '#app',
