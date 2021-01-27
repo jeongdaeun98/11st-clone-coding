d6cd664200e55cffe6e5c601858e1131faecf834
jeongdaeun98 <uneap@sk.com>
2021-01-27 14:31:08 +0900

    feat: add App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,63 @@
 <template>
   <div>
     <l-n-b></l-n-b>
+    <r-n-b></r-n-b>
     <my-header></my-header>
     <billboard></billboard>
+    <direct></direct>
+    <hot-focus></hot-focus>
+    <time-deal></time-deal>
+    <airlift></airlift>
+    <plan-banner></plan-banner>
+    <wide-banner></wide-banner>
+    <best-product></best-product>
+    <todays-chance></todays-chance>
+    <promotion></promotion>
+    <mds-recommendation></mds-recommendation>
+    <partner></partner>
+    <global-banner></global-banner>
+    <my-footer></my-footer>
   </div>
 </template>
 
 <script>
-import LNB from "~/src/components/LNB";
-import MyHeader from "~/src/components/MyHeader";
-import Billboard from "./components/Billboard";
+import LNB from "~/src/components/LNB"
+import MyHeader from "~/src/components/MyHeader"
+import Billboard from "./components/Billboard"
+import Airlift from "./components/Airlift"
+import RNB from "./components/RNB"
+import MyFooter from './components/MyFooter'
+import GlobalBanner from "./components/GlobalBanner"
+import BestProduct from './components/BestProduct'
+import Direct from './components/Direct'
+import MdsRecommendation from './components/MdsRecommendation'
+import HotFocus from './components/HotFocus'
+import TimeDeal from './components/TimeDeal'
+import PlanBanner from "./components/PlanBanner"
+import Promotion from "./components/Promotion"
+import TodaysChance from './components/TodaysChance'
+import WideBanner from './components/WideBanner'
+import Partner from "./components/Partner"
+
 export default {
   components: {
-    MyHeader,
     LNB,
-    Billboard
+    RNB,
+    MyHeader,
+    Billboard,
+    Airlift,
+    MyFooter,
+    GlobalBanner,
+    BestProduct,
+    Direct,
+    MdsRecommendation,
+    HotFocus,
+    TimeDeal,
+    PlanBanner,
+    Promotion,
+    TodaysChance,
+    WideBanner,
+    Partner
   }
 }
 </script>
